b4af5ea2c73779b834c60245b1cfdb8aef97c79f
ysW <yansanwoo@gmail.com>
2026-05-27 11:38:12 +0900

    test(conf): add unit test for conf

---
+++ src/tests/test_conf.py
@@ -0,0 +1,9 @@
+# tests/conftest.py
+import os
+import pytest
+
+# Global testing gatekeeper: Inject mock/dummy variables 
+# so tests stay isolated from active cloud billings or local network drops
+os.environ["GOOGLE_API_KEY"] = "mock_testing_key_abcdefg"
+os.environ["COHERE_API_KEY"] = "mock_cohere_key_123456"
+os.environ["LOCAL_OLLAMA_URL"] = "http://mock-localhost:11434"
